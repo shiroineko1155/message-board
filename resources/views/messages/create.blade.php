0b49eb608aa32af5c4ecce2a5088f8b7ce9b410e
Ruriko Gotou <odoruneko24@gmail.com>
2017-04-09 15:00:42 +0000

    bootstrap-design

---
+++ resources/views/messages/create.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+ <div class="row">
+     <div class="col-xs-6">
+ 
+ 
+ {!! Form::model($message, ['route' => 'messages.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('title', 'タイトル:') !!}
+                    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        
+            {!! Form::close() !!}
+    </div>
+</div>    
---
+++ resources/views/messages/edit.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+<div class="row">
+        <div class="col-xs-6">
+            {!! Form::model($message, ['route' => ['messages.update', $message->id], 'method' => 'put']) !!}
+                <div class="form-group">
+                    {!! Form::label('title', 'タイトル:') !!}
+                    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                <div class="form-group">
+                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+        
+            {!! Form::close() !!}
+        </div>
+    </div>
